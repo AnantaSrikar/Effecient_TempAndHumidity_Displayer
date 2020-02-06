863501f83e2b870f1eda4e5d39c059c9d9a654d8
Ananta Srikar Puranam <srikarananta01@gmail.com>
2020-02-06 12:40:35 +0530

    All working as expected

--- Live_temp_Humidity_sensor.ino
+++ Live_temp_Humidity_sensor.ino
@@ -8,12 +8,15 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // the necessary pins for LCD to work
 const int sensorPin = 7; // Input pin for LDR. Use 3.3V power supply for LDR
 int sensorValue = 0; // Variable to store the value coming from the LDR
 const int voltagePin = 8; // pin to control the power supply to the Humidity and Temp sensor
+const int vPinLCD = 9; // pin to control the power supply to the LCD
 
 void setup() {
   Serial.begin(9600);
   switchOffBuiltInLED();
   pinMode(voltagePin, OUTPUT);
+  pinMode(vPinLCD, OUTPUT);
   digitalWrite(voltagePin, HIGH);
+  digitalWrite(vPinLCD, HIGH);
   lcd.begin(16,2);
 }
 
@@ -25,7 +28,7 @@ void loop() {
   }
 
   else {
-    digitalWrite(voltagePin, LOW); //This is where power consumption is reduced
+    savePower();
   }
   
   delay(2000);  //IDK y but, if u reduce the time less than this, u start getting negative values for the temp and humidity, which we don't want
@@ -45,11 +48,27 @@ bool isLightOn(){
   return (!sensorValue);
 }
 
+void resetLCD(){
+  lcd.begin(16,2);  
+}
+
+void savePower(){   //This is where power consumption is reduced
+  digitalWrite(voltagePin, LOW);
+  digitalWrite(vPinLCD, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(2, LOW);
+}
+
 void giveTempAndHumidityValues(dht DHT){
-  
   DHT.read11(DHT11_PIN);
   digitalWrite(voltagePin, HIGH); //Ths is IMP
-  Serial.println("Temp : " + String(DHT.temperature) + " C\nHumidity : " + String(DHT.humidity) + "%\n\n");
+  digitalWrite(vPinLCD, HIGH);
+  resetLCD();
+  //Serial.println("Temp : " + String(DHT.temperature) + " C\nHumidity : " + String(DHT.humidity) + "%\n\n");
   lcd.clear(); //Clearing previous display
   lcd.setCursor(0,0);
   lcd.print("Temp = " + String(DHT.temperature) + " C");
